a1b3e8b5a6bda73665d92b84444863a6a936f026
Daniil Yarockii <yaroc.d@gmail.com>
2022-06-07 17:43:05 +0300

    - testing step

--- src/main.py
+++ src/main.py
@@ -26,6 +26,3 @@ def read_index(request: Request):
 async def apply_changes(state: StateJson = Depends(StateJson.from_request)):
     await state.synchronize_changes()
 
-
-if __name__ == '__main__':
-    uvicorn.run(g.app, host="0.0.0.0", port=8000, log_level="info")
