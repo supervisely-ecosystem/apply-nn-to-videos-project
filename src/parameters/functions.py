a4da2b3bc6728e3c1a7db7ece15bc5aedc535208
cxnt <pavelbartsits@gmail.com>
2022-12-12 21:16:03 +0400

    add preview for small videos under 30 frames

--- src/parameters/functions.py
+++ src/parameters/functions.py
@@ -23,12 +23,15 @@ def get_video_for_preview(state):
     video_name = selected_videos[0]
 
     video_info = [row for row in videos_table if row['name'] == video_name][0]
-
+    
     start_frame = 0
-    end_frame = frames_max[video_info['name']] + 1
-    while end_frame > frames_max[video_info['name']]:
-        start_frame = random.randint(frames_min[video_info['name']], frames_max[video_info['name']])
-        end_frame = start_frame + 29
+    if frames_max[video_info['name']] <= 30:
+        end_frame = frames_max[video_info['name']]
+    else:
+        end_frame = frames_max[video_info['name']] + 1
+        while end_frame > frames_max[video_info['name']]:
+            start_frame = random.randint(frames_min[video_info['name']], frames_max[video_info['name']])
+            end_frame = start_frame + 29
 
     return video_info['videoId'], (start_frame, end_frame)
 
