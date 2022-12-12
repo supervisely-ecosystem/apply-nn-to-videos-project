987f6f950d6b1f35c1dba4a53e5165f811be603e
cxnt <pavelbartsits@gmail.com>
2022-12-12 20:47:08 +0400

    fix dataset video processing

--- src/output_data/functions.py
+++ src/output_data/functions.py
@@ -14,15 +14,21 @@ from supervisely.app.fastapi import run_sync
 def restart(data, state):
     data["done6"] = False
 
+
 def shutdown_app():
     try:
         sly.app.fastapi.shutdown()
     except KeyboardInterrupt:
         sly.logger.info("Application shutdown successfully")
-        
-def init_project_remotely(project_name='ApplyNNtoVideoProject'):
-    project = g.api.project.create(g.workspace_id, project_name, type=sly.ProjectType.VIDEOS,
-                                   change_name_if_conflict=True)
+
+
+def init_project_remotely(project_name="ApplyNNtoVideoProject"):
+    project = g.api.project.create(
+        g.workspace_id,
+        project_name,
+        type=sly.ProjectType.VIDEOS,
+        change_name_if_conflict=True,
+    )
 
     meta = g.model_meta  # @TODO generate meta for multiclass
 
@@ -32,73 +38,98 @@ def init_project_remotely(project_name='ApplyNNtoVideoProject'):
 
 
 def upload_to_project(video_data, annotation: sly.VideoAnnotation, dataset_id):
-    video_hash = video_data['videoHash']
-    video_name = video_data['name']
+    video_hash = video_data["videoHash"]
+    video_name = video_data["name"]
     file_info = g.api.video.upload_hash(dataset_id, video_name, video_hash)
 
     g.api.video.annotation.append(file_info.id, annotation)
 
 
 def get_video_annotation(video_data, state) -> sly.VideoAnnotation:
-    frames_min = state['framesMin']
-    frames_max = state['framesMax']
-    frames_range = (frames_min[video_data['name']], frames_max[video_data['name']])
+    frames_min = state["framesMin"]
+    frames_max = state["framesMax"]
+    frames_range = (frames_min[video_data["name"]], frames_max[video_data["name"]])
 
-    with card_widgets.current_video_progress(message='Gathering Predictions from Model', total=1) as progress:
-        model_predictions = f.get_model_inference(state, video_id=video_data['videoId'], frames_range=frames_range)
+    with card_widgets.current_video_progress(
+        message="Gathering Predictions from Model", total=1
+    ) as progress:
+        model_predictions = f.get_model_inference(
+            state, video_id=video_data["videoId"], frames_range=frames_range
+        )
         progress.update(1)
 
     frame_to_annotation = f.frame_index_to_annotation(model_predictions, frames_range)
-    frame_to_annotation = f.filter_annotation_by_classes(frame_to_annotation, g.selected_classes_list)
+    frame_to_annotation = f.filter_annotation_by_classes(
+        frame_to_annotation, g.selected_classes_list
+    )
 
-    if state['applyTrackingAlgorithm'] is True:
+    if state["applyTrackingAlgorithm"] is True:
         with card_widgets.current_video_progress(
-                message=f'Applying tracking algorithm ({state["selectedTrackingAlgorithm"]})',
-                total=abs(frames_range[0] - frames_range[1]) + 1) as progress:
+            message=f'Applying tracking algorithm ({state["selectedTrackingAlgorithm"]})',
+            total=abs(frames_range[0] - frames_range[1]) + 1,
+        ) as progress:
 
-            return f.apply_tracking_algorithm_to_predictions(state=state, video_id=video_data['videoId'],
-                                                             frames_range=frames_range,
-                                                             frame_to_annotation=frame_to_annotation,
-                                                             tracking_algorithm=state["selectedTrackingAlgorithm"],
-                                                             pbar_cb=progress.update)
+            return f.apply_tracking_algorithm_to_predictions(
+                state=state,
+                video_id=video_data["videoId"],
+                frames_range=frames_range,
+                frame_to_annotation=frame_to_annotation,
+                tracking_algorithm=state["selectedTrackingAlgorithm"],
+                pbar_cb=progress.update,
+            )
     else:
         raise NotImplementedError
         # return f.get_annotation_from_predictions(frame_to_annotation)
 
 
 def annotate_videos(state):
-    selected_videos_names = state['selectedVideos']
+    selected_videos_names = state["selectedVideos"]
 
-    output_project_name = state['expId']
+    output_project_name = state["expId"]
     project_id = init_project_remotely(project_name=output_project_name)
 
-
     for ds_name in g.ds_video_map:
-        dataset = g.api.dataset.create(project_id, ds_name, change_name_if_conflict=True)
+        dataset = g.api.dataset.create(
+            project_id, ds_name, change_name_if_conflict=True
+        )
 
-        videos_table = DataJson()['videosTable']
+        videos_table = DataJson()["videosTable"]
 
-        selected_videos_data = [row for row in videos_table if row['name'] in selected_videos_names and row["name"] in g.ds_video_map[ds_name]]
+        selected_videos_data = [
+            row
+            for row in videos_table
+            if row["name"] in selected_videos_names
+            and row["name"] in g.ds_video_map[ds_name]
+            and row["dataset"] == ds_name
+        ]
 
-        for video_data in card_widgets.apply_nn_to_video_project_progress(selected_videos_data,
-                                                                          message='Inference Videos'):
+        for video_data in card_widgets.apply_nn_to_video_project_progress(
+            selected_videos_data, message=f"Inference Videos in dataset: {ds_name}"
+        ):
             try:
-                annotation: sly.VideoAnnotation = get_video_annotation(video_data, state)
+                annotation: sly.VideoAnnotation = get_video_annotation(
+                    video_data, state
+                )
                 upload_to_project(video_data, annotation, dataset.id)
 
             except Exception as ex:
-                raise RuntimeError(f'Error while processing: {video_data["name"]}:'
-                                   f'{ex}')
+                raise RuntimeError(
+                    f'Error while processing: {video_data["name"]}:' f"{ex}"
+                )
 
     res_project = g.api.project.get_info_by_id(project_id)
-    DataJson().update({
-        'dstProjectId': res_project.id,
-        'dstProjectName': res_project.name,
-        'dstProjectPreviewUrl': g.api.image.preview_url(res_project.reference_image_url, 100, 100),
-    })
+    DataJson().update(
+        {
+            "dstProjectId": res_project.id,
+            "dstProjectName": res_project.name,
+            "dstProjectPreviewUrl": g.api.image.preview_url(
+                res_project.reference_image_url, 100, 100
+            ),
+        }
+    )
 
-    DataJson()['done6'] = True
-    DataJson()['annotatingStarted'] = False
+    DataJson()["done6"] = True
+    DataJson()["annotatingStarted"] = False
 
     run_sync(DataJson().synchronize_changes())
     shutdown_app()
--- src/sly_globals.py
+++ src/sly_globals.py
@@ -14,17 +14,19 @@ from supervisely.app.fastapi import create, Jinja2Templates
 app_root_directory = str(pathlib.Path(__file__).parent.absolute().parents[0])
 sly.logger.info(f"Root source directory: {app_root_directory}")
 
-temp_dir = os.path.join(app_root_directory, 'temp')
-preview_frames_path = os.path.join(temp_dir, 'preview_frames')
+temp_dir = os.path.join(app_root_directory, "temp")
+preview_frames_path = os.path.join(temp_dir, "preview_frames")
 
 # for debug
 # from dotenv import load_dotenv
-# load_dotenv(os.path.join(app_root_directory, 'debug.env'))
-# load_dotenv(os.path.join(app_root_directory, 'secret_debug.env'), override=True)
+# load_dotenv(os.path.join(app_root_directory, "debug.env"))
+# load_dotenv(os.path.expanduser("~/supervisely.env"))
+
 
 class AnnotatorModes:
-    DIRECT = 'direct'
-    DEEPSORT = 'deepsort'
+    DIRECT = "direct"
+    DEEPSORT = "deepsort"
+
 
 api: sly.Api = sly.Api.from_env()
 
@@ -32,34 +34,53 @@ app = FastAPI()
 sly_app = create()
 
 app.mount("/sly", sly_app)
-app.mount("/static", StaticFiles(directory=os.path.join(app_root_directory, 'static')), name="static")
+app.mount(
+    "/static",
+    StaticFiles(directory=os.path.join(app_root_directory, "static")),
+    name="static",
+)
 
-templates_env = Jinja2Templates(directory=os.path.join(app_root_directory, 'templates'))
+templates_env = Jinja2Templates(directory=os.path.join(app_root_directory, "templates"))
 
-supported_model_types = ['Semantic Segmentation', 'Object Detection', 'Instance Segmentation', 'Tracking']
-model_types_without_tracking = ['Semantic Segmentation', 'Object Detection', 'Instance Segmentation']
+supported_model_types = [
+    "Semantic Segmentation",
+    "Object Detection",
+    "Instance Segmentation",
+    "Tracking",
+]
+model_types_without_tracking = [
+    "Semantic Segmentation",
+    "Object Detection",
+    "Instance Segmentation",
+]
 
-DataJson()['current_step'] = 1
-DataJson()['mode'] = AnnotatorModes.DIRECT
+DataJson()["current_step"] = 1
+DataJson()["mode"] = AnnotatorModes.DIRECT
 
 
-owner_id = int(os.environ['context.userId'])
-team_id = int(os.environ['context.teamId'])
-project_id = int(os.environ['modal.state.slyProjectId'])
-workspace_id = int(os.environ['context.workspaceId'])
+owner_id = int(os.environ["context.userId"])
+team_id = int(os.environ["context.teamId"])
+project_id = int(os.environ["modal.state.slyProjectId"])
+workspace_id = int(os.environ["context.workspaceId"])
 
 project_info = api.project.get_info_by_id(project_id)
-project_meta: sly.ProjectMeta = sly.ProjectMeta.from_json(api.project.get_meta(project_id))
+project_meta: sly.ProjectMeta = sly.ProjectMeta.from_json(
+    api.project.get_meta(project_id)
+)
 
 ds_video_map = None
 model_info = None
 model_meta: sly.ProjectMeta = None
-device = torch.device('cuda:0') if torch.cuda.is_available() and torch.cuda.device_count() > 0 else torch.device('cpu')
+device = (
+    torch.device("cuda:0")
+    if torch.cuda.is_available() and torch.cuda.device_count() > 0
+    else torch.device("cpu")
+)
 
 
 DataJson()["ownerId"] = owner_id
 DataJson()["teamId"] = team_id
-DataJson()['instanceAddress'] = os.getenv('SERVER_ADDRESS')
+DataJson()["instanceAddress"] = os.getenv("SERVER_ADDRESS")
 
 StateJson()["activeStep"] = 1
 StateJson()["restartFrom"] = None
