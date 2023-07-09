d980b361236eb39ca11dac94d8ee8830ea0e5cfa
Pavel Bartsits <48913536+cxnt@users.noreply.github.com>
2023-07-09 20:14:06 +0400

    add progress bar for annotation upload (#22)
    * add progress bar for annotation upload
    
    * add progress
    
    * update supervisely version
    
    * Delete requirements.txt
    
    * Update config.json
    
    * Update Dockerfile
    
    * Update dev_requirements.txt

--- src/output_data/functions.py
+++ src/output_data/functions.py
@@ -37,12 +37,12 @@ def init_project_remotely(project_name="ApplyNNtoVideoProject"):
     return project.id
 
 
-def upload_to_project(video_data, annotation: sly.VideoAnnotation, dataset_id):
+def upload_to_project(video_data, annotation: sly.VideoAnnotation, dataset_id, progress):
     video_hash = video_data["videoHash"]
     video_name = video_data["name"]
     file_info = g.api.video.upload_hash(dataset_id, video_name, video_hash)
-
-    g.api.video.annotation.append(file_info.id, annotation)
+    progress_cb = progress(message="Uploading annotation", total=len(annotation.figures))
+    g.api.video.annotation.append(file_info.id, annotation, progress_cb=progress_cb)
 
 
 def get_video_annotation(video_data, state) -> sly.VideoAnnotation:
@@ -51,7 +51,10 @@ def get_video_annotation(video_data, state) -> sly.VideoAnnotation:
     frames_range = (frames_min[video_data["name"]], frames_max[video_data["name"]])
 
     model_predictions = f.get_model_inference(
-        state, video_id=video_data["videoId"], frames_range=frames_range, progress_widget=card_widgets.current_video_progress
+        state,
+        video_id=video_data["videoId"],
+        frames_range=frames_range,
+        progress_widget=card_widgets.current_video_progress,
     )
 
     frame_to_annotation = f.frame_index_to_annotation(model_predictions, frames_range)
@@ -64,7 +67,6 @@ def get_video_annotation(video_data, state) -> sly.VideoAnnotation:
             message=f'Applying tracking algorithm ({state["selectedTrackingAlgorithm"]})',
             total=abs(frames_range[0] - frames_range[1]) + 1,
         ) as progress:
-
             return f.apply_tracking_algorithm_to_predictions(
                 state=state,
                 video_id=video_data["videoId"],
@@ -75,7 +77,9 @@ def get_video_annotation(video_data, state) -> sly.VideoAnnotation:
             )
     else:
         obj_classes = g.model_meta.obj_classes
-        return annotations_to_video_annotation(frame_to_annotation, obj_classes, video_data['frame_shape'])
+        return annotations_to_video_annotation(
+            frame_to_annotation, obj_classes, video_data["frame_shape"]
+        )
 
 
 def annotate_videos(state):
@@ -85,9 +89,7 @@ def annotate_videos(state):
     project_id = init_project_remotely(project_name=output_project_name)
 
     for ds_name in g.ds_video_map:
-        dataset = g.api.dataset.create(
-            project_id, ds_name, change_name_if_conflict=True
-        )
+        dataset = g.api.dataset.create(project_id, ds_name, change_name_if_conflict=True)
 
         videos_table = DataJson()["videosTable"]
 
@@ -103,7 +105,9 @@ def annotate_videos(state):
             selected_videos_data, message=f"Inference Videos in dataset: {ds_name}"
         ):
             annotation: sly.VideoAnnotation = get_video_annotation(video_data, state)
-            upload_to_project(video_data, annotation, dataset.id)
+            upload_to_project(
+                video_data, annotation, dataset.id, card_widgets.current_video_progress
+            )
 
     res_project = g.api.project.get_info_by_id(project_id)
     DataJson().update(
@@ -130,17 +134,26 @@ def stop_annotate_videos(state):
 
     if g.inference_session:
         g.inference_session.stop_async_inference()
-    
+
     if g.inference_request_uuid:
-        g.api.task.send_request(state['sessionId'], "stop_inference", data={"inference_request_uuid": g.inference_request_uuid})
+        g.api.task.send_request(
+            state["sessionId"],
+            "stop_inference",
+            data={"inference_request_uuid": g.inference_request_uuid},
+        )
 
 
-def annotations_to_video_annotation(frame_to_annotation: dict, obj_classes: sly.ObjClassCollection, video_shape: tuple):
+def annotations_to_video_annotation(
+    frame_to_annotation: dict, obj_classes: sly.ObjClassCollection, video_shape: tuple
+):
     name2vid_obj_cls = {x.name: sly.VideoObject(x) for x in obj_classes}
     video_obj_classes = sly.VideoObjectCollection(list(name2vid_obj_cls.values()))
     frames = []
-    for idx, ann in frame_to_annotation.items():
-        ann : sly.Annotation
+
+    for idx, ann in card_widgets.current_video_progress(
+        frame_to_annotation.items(), message="Processing annotation"
+    ):
+        ann: sly.Annotation
         figures = []
         for label in ann.labels:
             vid_obj = name2vid_obj_cls[label.obj_class.name]
@@ -149,4 +162,5 @@ def annotations_to_video_annotation(frame_to_annotation: dict, obj_classes: sly.
         frames.append(sly.Frame(idx, figures))
     frames_coll = sly.FrameCollection(frames)
     video_ann = sly.VideoAnnotation(video_shape, len(frames_coll), video_obj_classes, frames_coll)
+    sly.logger.info(f"Annotation has been processed: {len(frame_to_annotation)} frames")
     return video_ann
